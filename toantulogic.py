d8741cbb3d1d9f02923949cd1d3762cbb0e2afe3
Phạm Gia Khánh <20230797@eaut.edu.vn>
2026-03-27 18:36:08 +0700

    Add files via upload

---
+++ toantulogic.py
@@ -0,0 +1,41 @@
+# Khai báo biến trực tiếp
+_a = 16
+_b = 3
+_c = 5
+
+# Số học
+print(_a + _b)
+print(_a - _c)
+print(_b * _c)
+print(_a / _b)
+print(_b ** _c)
+
+# Quan hệ
+print(_a > _b)
+print(_b < _c)
+print(_a == _c)
+print(_a != _b)
+
+# Gán
+_a += _b
+print(_a)
+_a /= _b
+print(_a)
+_a *= _c
+print(_a)
+
+# Trả lại giá trị cũ cho _a để tính tiếp
+_a = 16 
+
+# Logic
+print((_a > _b) and (_b < _c))
+print((_a == 10) or (_c == 5))
+print(not (_a == 16))
+
+# Thao tác bit
+print(_a & _b)
+print(_a | _b)
+print(~_a)
+print(_a ^ _b)
+print(_a << 3)
+print(_a >> 2)
